2703c9a6998e1f3e1bfbd785770650ca3a83af7f
Rigoberto Obregon <robregon_ifds@TKH-M1-Air-24.lan>
2022-11-28 21:19:15 -0800

    Trying gitignore

--- extract.py
+++ extract.py
@@ -1,7 +1,16 @@
-from config.py import key
+from config import key
+import requests
+import time
 
+stocks = ["GOOG", "AMZN", "COUR", "NFLX", "META"]
 
-url=f"https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2021-07-22/2021-07-22?adjusted=true&sort=asc&limit=120&apiKey={key}"
+for stock in stocks:
+    stocks_url = f"https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2021-03-29/2022-03-25?adjusted=true&sort=asc&limit=120&apiKey={key}"
+    data = requests.get(stocks_url)
 
+    results = data.json()["results"]
+    for result in results:
+        print(f"stock: {stock}, date: {int(int(result['t']))} closing: {result['c']}")
+        #print(f"stock: {stock}, date: {time.strftime('%y-%m-%d', int(int(result['t'])/1000))} closing: {result['c']}")
 
-
+#print(time.strftime('%y-%m-%d', ))
